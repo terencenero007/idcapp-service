de03f8e14d2474b7e59873d68778b890256bcb1c
unknown <p10267646@COS-LPT-DDAS.capita.co.in>
2016-04-19 14:04:10 +0530

    compilation error corrected

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.get('/ping', function (req, res) {
   res.send('Hello World');
 });
 
-abc........
-
 // Listen http server on configured  port
 app.listen(process.env.PORT || nconf.get('NODE_SERVER_PORT'), function () {
   console.log('Hello World app listening on port ' + nconf.get('NODE_SERVER_PORT'));
