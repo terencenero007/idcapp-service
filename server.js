62aea0935678ac78143964c364073dc36a1270fb
unknown <p10267646@COS-LPT-DDAS.capita.co.in>
2016-04-19 15:30:34 +0530

    Corrected port

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.get('/ping', function (req, res) {
 });
 
 // Listen http server on configured  port
-app.listen(nconf.get('NODE_SERVER_PORT'), function () {
-  console.log('Hello World app listening on port ' + nconf.get('NODE_SERVER_PORT'));
+app.listen(nconf.get('PORT'), function () {
+  console.log('Hello World app listening on port ' + nconf.get('PORT'));
 });
