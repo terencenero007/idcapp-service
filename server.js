9c65cc990923223a36403cc4ecf48a15c208b7fa
unknown <p10267646@COS-LPT-DDAS.capita.co.in>
2016-04-19 10:47:27 +0530

    Added nconf. Also making build error to check codeship

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ var express = require('express');
 var app = express();
 
 // Load configuration
-var config = require('./config.json');
+var nconf = require('nconf');
+nconf.env()
+   .file({file: './config.json'});
 
 // ping API
 app.get('/ping', function (req, res) {
   res.send('Hello World');
 });
 
+abb...
+
 // Listen http server on configured  port
-app.listen(process.env.PORT || config.PORT, function () {
-  console.log('Hello World app listening on port 3000');
+app.listen(process.env.PORT || nconf.get('NODE_SERVER_PORT'), function () {
+  console.log('Hello World app listening on port ' + nconf.get('NODE_SERVER_PORT'));
 });
