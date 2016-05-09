65eb65a44ee915bad71d6ab7e0e931a620c8147f
unknown <p10267646@COS-LPT-DDAS.capita.co.in>
2016-05-09 16:28:45 +0530

    Added testcases for message service

--- tests/unit/messageService.test.js
+++ tests/unit/messageService.test.js
@@ -4,18 +4,22 @@
 
 // Load chai library to aseert the testcases
 var chai = require('chai');
+chai.should();
+chai.use(require('chai-things'));
 var expect = chai.expect; 
 
 // Load messageService to run the test cases
 var messageService = require('../../src/services/messageService');
 
 // test1
-// Currently messageService is returning temporary data. So just test number of items for now
+// Currently messageService is returning temporary data. 
+// Validate number of messages and its property keys
 describe('MessageService', function() {
-  it('getMessages() should return 2 items', function() {
+  it('getMessages() should return 2 messages with "id", "title", "content", "timestamp" properties', function() {
     var currentMessageCount = 2;  
     var messages = messageService.getMessages();
-    expect(messageService.getMessages()).to.have.lengthOf(currentMessageCount); 
+    expect(messages).to.have.lengthOf(currentMessageCount);
+    messages.should.all.include.keys(["id", "title", "content", "timestamp"]);  
   });
 });
 
